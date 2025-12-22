0f05757cc893e2df03ea3257d034fe23c87b6a8e
viciious <vluchits@gmail.com>
2025-12-22 11:12:05 +0300

    Update colormap32x.py

--- colormap32x.py
+++ colormap32x.py
@@ -47,9 +47,39 @@ remap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 2
 # "16:24=49:57", "25:40=128:143", "41:44=148:151", "45:47=236:238", "56:61=59:64", "63:77=65:79", "78:79=5:6"
 # water to slime
 # "203:207=147:151", "240:243=76:79", "244:244=239:239", "245:245=1:1"
-remap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 49, 50, 51, 52, 53, 54, 55, 56, 57, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 148, 149, 150, 151, 236, 237, 238, 48, 49, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 62, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 5, 6, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 147, 148, 149, 150, 151, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 76, 77, 78, 79, 239, 1, 246, 247, 239, 249, 250, 251, 252, 253, 254, 255]
+# intense red to dark blue
+# "186:186=244:244", "187:187=247:247"
+
+#remap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 49, 50, 51, 52, 53, 54, 55, 56, 57, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 148, 149, 150, 151, 236, 237, 238, 48, 49, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 62, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 5, 6, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 147, 148, 149, 150, 151, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 76, 77, 78, 79, 239, 1, 246, 247, 239, 249, 250, 251, 252, 253, 254, 255]
+
+#remap = [0, 1, 2, 236, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 49, 50, 51, 52, 53, 54, 55, 56, 57, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 148, 149, 150, 151, 236, 237, 238, 48, 49, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 62, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 5, 6, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 147, 148, 149, 150, 151, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 76, 77, 78, 79, 239, 1, 246, 247, 239, 249, 250, 251, 252, 253, 254, 255]
+
+#remap = [0, 1, 2, 236, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 49, 50, 51, 52, 53, 54, 55, 56, 57, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 148, 149, 150, 151, 236, 237, 238, 48, 49, 50, 51, 52, 53, 54, 55, 59, 60, 61, 62, 63, 64, 62, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 5, 6, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 244, 247, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 147, 148, 149, 150, 151, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 76, 77, 78, 79, 239, 1, 246, 247, 239, 249, 250, 251, 252, 253, 254, 255]
 
 dither = False
+dither_diff = 12
+remapstr = ""
+
+def ranges2map(ranges):
+    ranges = ranges.split(",")
+    mapping = [0]*256
+    for i in range(256):
+       mapping[i] = i
+
+    for r in ranges:
+        r = r.strip(' ').strip('"').strip("\n").strip('"')
+        l, r = tuple(r.split('='))
+        s1, e1 = tuple(l.split(':'))
+        s2, e2 = tuple(r.split(':'))
+
+        s1, e1 = int(s1), int(e1)+1
+        s2, e2 = int(s2), int(e2)+1
+
+        i = s1
+        while i < e1:
+            mapping[i] = s2+(i-s1)
+            i += 1
+    return mapping
 
 def read_palette(filename):
     """Read palette from file and return a list of tuples containing
@@ -71,6 +101,39 @@ def read_palette(filename):
 
     return colors
 
+def dist(c1, c2):
+    r = c1[0] - c2[0]
+    g = c1[1] - c2[1]
+    b = c1[2] - c2[2]
+    return math.sqrt(r*r + g*g + b*b)
+
+def color_linearize(c):
+    if c <= 0.04045:
+        return c / 12.92
+    return math.pow((( c + 0.055)/1.055), 2.4)
+
+def rgb_y(c):
+    r = color_linearize(c[0] / 255.0)
+    g = color_linearize(c[1] / 255.0)
+    b = color_linearize(c[2] / 255.0)
+    return 0.2126 * r + 0.7152 * g + 0.0722 * b
+
+def y_lstar(y):
+    if y <= (216.0/24389):
+        return y * (24389.9/27)
+    return math.pow(y,(1.0/3)) * 116 - 16
+
+# https://www.compuphase.com/cmetric.htm
+def rgb_distance(c1, c2):
+    #return dist(c1, c2)
+    rmean = ( c1[0] + c2[0] ) / 2.0
+    r = c1[0] - c2[0]
+    g = c1[1] - c2[1]
+    b = c1[2] - c2[2]
+    return math.sqrt((((512.0+rmean)*r*r) / 256.0) + 4.0*g*g + (((767-rmean)*b*b)/256.0))
+
+def rgb_lstar(c):
+    return y_lstar(rgb_y(c)) / 100.0
 
 # Return closest palette entry to the given RGB triple
 
@@ -79,39 +142,6 @@ def search_palette(palette, target, preserve_lum = True, ignore=-1, ignore2=-1):
        color to the given color, returning an index into the
        palette of the color that best matches."""
 
-    def dist(c1, c2):
-        r = c1[0] - c2[0]
-        g = c1[1] - c2[1]
-        b = c1[2] - c2[2]
-        return math.sqrt(r*r + g*g + b*b)
-
-    def color_linearize(c):
-        if c <= 0.04045:
-            return c / 12.92
-        return math.pow((( c + 0.055)/1.055), 2.4)
-
-    def rgb_y(c):
-        r = color_linearize(c[0] / 255.0)
-        g = color_linearize(c[1] / 255.0)
-        b = color_linearize(c[2] / 255.0)
-        return 0.2126 * r + 0.7152 * g + 0.0722 * b
-
-    def y_lstar(y):
-        if y <= (216.0/24389):
-            return y * (24389.9/27)
-        return math.pow(y,(1.0/3)) * 116 - 16
-
-    # https://www.compuphase.com/cmetric.htm
-    def rgb_distance(c1, c2):
-        #return dist(c1, c2)
-        rmean = ( c1[0] + c2[0] ) / 2.0
-        r = c1[0] - c2[0]
-        g = c1[1] - c2[1]
-        b = c1[2] - c2[2]
-        return math.sqrt((((512.0+rmean)*r*r) / 256.0) + 4.0*g*g + (((767-rmean)*b*b)/256.0))
-
-    def rgb_lstar(c):
-        return y_lstar(rgb_y(c)) / 100.0
     
     best_diff = None
     best_index = None
@@ -126,7 +156,7 @@ def search_palette(palette, target, preserve_lum = True, ignore=-1, ignore2=-1):
 
         diff = rgb_distance(target, color)
 
-        if best_index is None or diff < best_diff:
+        if best_index is None or diff <= best_diff:
             best_diff2 = best_diff
             best_index2 = best_index
             best_diff = diff
@@ -143,20 +173,39 @@ def search_palette(palette, target, preserve_lum = True, ignore=-1, ignore2=-1):
     return best_index
 
 
-def generate_colormap(colors, palette, preserve_lum = True):
+def generate_colormaps(colors, palette, preserve_lum = True, Foo = False):
     """Given a list of colors, translate these into indexes into
        the given palette, finding the nearest color where an exact
        match cannot be found."""
     result = []
+    result2 = []
+    preserve_lum = True
 
-    for color in colors:
-        index = search_palette(palette, color, preserve_lum)
+    for target in colors:
+        index = search_palette(palette, target, preserve_lum)
+        index2 = index
+        color = palette[index]
+        diff = rgb_distance(target, color)
+        if diff > dither_diff:
+            step = 4
+            y1 = rgb_lstar(target)
+            y2 = rgb_lstar(color)
+            if y1 > y2:
+                color2 = (round(color[0]+step), round(color[1]+step), round(color[2]+step))
+            else:
+                color2 = (round(color[0]-step), round(color[1]-step), round(color[2]-step))
+            index2 = search_palette(palette, color2, preserve_lum)
+            color2 = palette[index2]
         result.append(index)
+        result2.append(index2)
 
     for i, r in enumerate(remap):
         result[i] = result[r]
 
-    return result
+    for i, r in enumerate(remap):
+        result2[i] = result2[r]
+
+    return (result, result2)
 
 
 def swap_colormap(colormap):
@@ -164,22 +213,6 @@ def swap_colormap(colormap):
     for i in range(128, len(colormap), 256):
         result += colormap[i:i+128]
         result += colormap[i-128:i]
-    return result
-
-
-def generate_dithered_colormap(colormap, dithered):
-    global dither
-
-    result = []
-    if dither:
-        for c in colormap:
-            result.append(dithered[c][3])
-            result.append(dithered[c][4])
-    else:
-        for c in colormap:
-            result.append(dithered[c][3])
-            result.append(dithered[c][3])
-
     return result
 
 
@@ -212,9 +245,9 @@ def blend_colors(colors1, colors2, factor=0.5):
         c2 = colors2[index]
         result.append(
             (
-                c2[0] * factor + c1[0] * (1 - factor),
-                c2[1] * factor + c1[1] * (1 - factor),
-                c2[2] * factor + c1[2] * (1 - factor),
+                round(c2[0] * factor + c1[0] * (1 - factor)),
+                round(c2[1] * factor + c1[1] * (1 - factor)),
+                round(c2[2] * factor + c1[2] * (1 - factor)),
             )
         )
 
@@ -241,12 +274,16 @@ def solid_color_list(color):
     return [color] * 256
 
 
-def output_colormap(colormap):
+def output_colormap(colormap, colormap2):
     """Output the given palette to stdout."""
-    for c in colormap:
-        x = struct.pack("B", c)
+    for i in range(len(colormap)):
+        c = colormap[i]
+        c2 = colormap2[i]
+        x = struct.pack("<B", c)
         os.write(sys.stdout.fileno(), x)
-
+        if dither:
+            x2 = struct.pack("<B", c2)
+            os.write(sys.stdout.fileno(), x2)
 
 def print_palette(colors):
     for y in range(16):
@@ -256,55 +293,6 @@ def print_palette(colors):
             print("#%02x%02x%02x" % color)
 
         print()
-
-def dithered_palette(palette):
-    global dither
-
-    dithered = []
-    for i, c in enumerate(palette):
-        # round to 32X palette
-        r, g, b = c[0], c[1], c[2]
-        if (r & 7) == 7 and (g & 7) == 7 and (b & 7) == 7:
-            r = (r + 1) & ~7
-            if r > 255:
-                r = 255
-            g = (g + 1) & ~7
-            if g > 255:
-                g = 255
-            b = (b + 1) & ~7
-            if b > 255:
-                b = 255
-        else:
-            r = r & ~7
-            g = g & ~7
-            b = b & ~7
-
-        c1 = c2 = i
-        grey = ((0.3 * r) + (0.59 * g) + (0.11 * b))
-        #if random.randint(0, 255) < grey * 0.9:
-        if dither:
-            if grey > 64:
-                c1 = search_palette(palette, (r, g, b), i)
-                if not c1:
-                    c1 = c2 = i
-                elif max(abs(palette[c1][0] - r), abs(palette[c1][1] - g), abs(palette[c1][2] - b)) < 5:
-                    c1 = c2 = i
-                else:
-                    c2 = search_palette(palette, (r, g, b), c1)
-                    if not c2:
-                        c2 = i
-
-                    if c1 and min(abs(palette[c1][0] - r), abs(palette[c1][1] - g), abs(palette[c1][2] - b)) > 8:
-                        c1 = i
-                    if c2 and min(abs(palette[c2][0] - r), abs(palette[c2][1] - g), abs(palette[c2][2] - b)) > 8:
-                        c2 = i
-
-                if random.randint(0, 100) > 50:
-                    t = c1
-                    c1 = c2
-                    c2 = t
-        dithered.append((r, g, b, c1, c2))
-    return dithered
 
 def parse_color_code(s):
     """Parse a color code in HTML color code format, into an RGB
@@ -316,7 +304,7 @@ def parse_color_code(s):
 
 def set_parameter(name, value):
     """Set configuration value, from command line parameters."""
-    global dark_color, tint_color, tint_frac, tint_bright, dither
+    global dark_color, tint_color, tint_frac, tint_bright, dither, remap, dither_diff
 
     if name == "dark_color":
         dark_color = parse_color_code(value)
@@ -328,6 +316,11 @@ def set_parameter(name, value):
         tint_bright = float(value)
     elif name == "dither":
         dither = True
+    elif name == "dither_diff":
+        dither_diff = int(value)
+    elif name == "remap":
+        with open(value) as f:
+            remap = ranges2map(f.readlines()[0])
     else:
         raise Exception("Unknown parameter: '%s'" % name)
 
@@ -349,8 +342,6 @@ if playpal_filename is None:
 
 palette = read_palette(playpal_filename)
 colors = palette
-dithered = dithered_palette(palette)
-
 # Apply tint, if enabled.
 # The tint is intentionally applied *before* the darkening effect is
 # applied. This allows us to darken to a different color than the tint
@@ -366,6 +357,12 @@ dark = solid_color_list(dark_color)
 
 close_range = 0
 
+def gernerate_colormap(colors, palette, preserve_lum = True, Foo = False):
+    colormap, colormap2 = generate_colormaps(colors, palette, preserve_lum, Foo)
+    colormap = swap_colormap(colormap)
+    colormap2 = swap_colormap(colormap2)
+    output_colormap(colormap, colormap2) 
+   
 for i in range(32):
     darken_factor = i - close_range
     if darken_factor < 0:
@@ -377,7 +374,7 @@ for i in range(32):
             darken_factor = 0
 
     darkened_colors = blend_colors(dark, colors, darken_factor)
-    output_colormap(generate_dithered_colormap(swap_colormap(generate_colormap(darkened_colors, palette, False)), dithered))
+    gernerate_colormap(darkened_colors, palette, True, i == 0)
 
 #tinted_colors = blend_colors(
 #    palette, tint_colors(colors, (0, 0, 255), tint_bright), tint_frac
@@ -386,10 +383,11 @@ for i in range(32):
 
 # Inverse color map for invulnerability effect.
 inverse_colors = invert_colors(palette)
-output_colormap(generate_dithered_colormap(swap_colormap(generate_colormap(inverse_colors, palette, False)), dithered))
+gernerate_colormap(inverse_colors, palette, False)
 
 # Last colormap is all black, and is actually unused in Vanilla Doom
 # (it was mistakenly included by the dcolors.c utility). It's
 # strictly unneeded, though some utilities (SLADE) do not detect a
 # lump as a COLORMAP unless it is the right length.
 #output_colormap(generate_colormap(dark, palette))
+
